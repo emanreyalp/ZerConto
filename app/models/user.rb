5846363e8730b94371efa9923e080ef362f65923
Gergely Dömök <emanreyalp@users.noreply.github.com>
2019-04-16 08:55:00 +0200

    Fix superior-employees connection

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_one :superior, class_name: :User
+  belongs_to :superior, class_name: :User, optional: true
   has_many :employees, class_name: :User, foreign_key: :superior_id
   has_many :locations_user, inverse_of: :user
   has_many :locations, through: :locations_user, dependent: :destroy
